ff101805c3d687ea7725bbd54bc1cfb18f309e31
Anu Thomas Chandy <anuamd@hotmail.com>
2017-12-10 21:01:42 -0800

    Full fluent coverage for storage features available in api-version 2017-10-01 (#87)
    * Enabling fluent layer for new storage features[network rules, file encryption, httpsOnly, storage skus]
    
    * Tests for new storage features [network rules, encryption]
    
    * Handling NPE in StorageNetworkRulesHelper
    
    * Re-recording tests affected by the 2017-10-01 storage upgrade
    
    * [Sampel update]: adding storage V2 code

--- azure-mgmt-storage/src/main/java/com/microsoft/azure/management/storage/Usages.java
+++ azure-mgmt-storage/src/main/java/com/microsoft/azure/management/storage/Usages.java
@@ -7,11 +7,17 @@
 package com.microsoft.azure.management.storage;
 
 import com.microsoft.azure.management.apigeneration.Fluent;
+import com.microsoft.azure.management.resources.fluentcore.arm.models.HasManager;
 import com.microsoft.azure.management.resources.fluentcore.collection.SupportsListing;
+import com.microsoft.azure.management.resources.fluentcore.model.HasInner;
+import com.microsoft.azure.management.storage.implementation.StorageManager;
+import com.microsoft.azure.management.storage.implementation.UsagesInner;
 
 /**
  * Entry point for storage resource usage management API.
  */
 @Fluent
-public interface Usages extends SupportsListing<StorageUsage> {
+public interface Usages extends SupportsListing<StorageUsage>,
+        HasInner<UsagesInner>,
+        HasManager<StorageManager> {
 }

9302fac70dc30c5db7119b440fecb6ebcb6320c4
lenala <lena.lakhno@gmail.com>
2018-01-10 14:13:44 -0800

    javadoc

--- azure-mgmt-batchai/src/main/java/com/microsoft/azure/management/batchai/ToolTypeSettings.java
+++ azure-mgmt-batchai/src/main/java/com/microsoft/azure/management/batchai/ToolTypeSettings.java
@@ -46,16 +46,32 @@ public interface ToolTypeSettings {
                     ToolTypeSettings.DefinitionStages.WithCommandLineArgs<WithAttach<ParentT>> {
             }
 
-            interface WithAttachAndPythonInterpreter<ParentT> extends WithAttach<ParentT> {
-                WithAttach<ParentT> withPythonInterpreterPath(String path);
+            interface WithAttachAndPythonInterpreter<ParentT> extends WithAttach<ParentT>,
+                    ToolTypeSettings.DefinitionStages.WithPythonInterpreter<WithAttach<ParentT>> {
             }
 
+            /**
+             * The first stage of the Microsoft Cognitive Toolkit settings definition.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface Blank<ParentT> extends WithLanguageType<ParentT> {
             }
 
+            /**
+             * Specifies the language type and script/config file path to use for Microsoft Cognitive Toolkit job.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface WithLanguageType<ParentT> {
+                /**
+                 * @param configFilePath path of the config file
+                 * @return the next stage of the definition
+                 */
                 WithAttach<ParentT> withBrainScript(String configFilePath);
 
+                /**
+                 * @param pythonScriptFilePath the path and file name of the python script to execute the job
+                 * @return the next stage of the definition
+                 */
                 WithAttachAndPythonInterpreter<ParentT> withPythonScriptFile(String pythonScriptFilePath);
             }
         }
@@ -71,7 +87,7 @@ public interface ToolTypeSettings {
             HasInner<TensorFlowSettings> {
 
         /**
-         * Definition of Tensorflow job settings.
+         * Definition of TensorFlow job settings.
          *
          * @param <ParentT> the stage of the parent definition to return to after attaching this definition
          */
@@ -98,10 +114,22 @@ public interface ToolTypeSettings {
                 WithAttach<ParentT> withParameterServerCount(int parameterServerCount);
             }
 
+            /**
+             * The first stage of the TensorFlow settings definition.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface Blank<ParentT> extends WithPython<ParentT> {
             }
 
+            /**
+             * Specifies python script file path.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface WithPython<ParentT> {
+                /**
+                 * @param pythonScriptFilePath the path and file name of the python script to execute the job
+                 * @return the next stage of the definition
+                 */
                 WithMasterCommandLineArgs<ParentT> withPythonScriptFile(String pythonScriptFilePath);
             }
 
@@ -145,12 +173,28 @@ public interface ToolTypeSettings {
                     ToolTypeSettings.DefinitionStages.WithPythonInterpreter<WithAttach<ParentT>> {
             }
 
+            /**
+             * The first stage of the Caffe settings definition.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface Blank<ParentT> extends WithFileType<ParentT> {
             }
 
+            /**
+             * Specifies the path and file name of the python script to execute the job or the path of the config file.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface WithFileType<ParentT> {
+                /**
+                 * @param configFilePath the path of the config file
+                 * @return the next stage of the definition
+                 */
                 WithAttach<ParentT> withConfigFile(String configFilePath);
 
+                /**
+                 * @param pythonScriptFilePath
+                 * @return the next stage of the definition
+                 */
                 WithAttachAndPythonInterpreter<ParentT> withPythonScriptFile(String pythonScriptFilePath);
             }
         }
@@ -189,9 +233,17 @@ public interface ToolTypeSettings {
                     ToolTypeSettings.DefinitionStages.WithPythonInterpreter<WithAttach<ParentT>> {
             }
 
+            /**
+             * The first stage of the Caffe2 settings definition.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface Blank<ParentT> extends WithPython<ParentT> {
             }
 
+            /**
+             * Specifies python script file path to execute the job.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface WithPython<ParentT> {
                 WithAttachAndPythonInterpreter<ParentT> withPythonScriptFile(String pythonScriptFilePath);
             }
@@ -232,9 +284,17 @@ public interface ToolTypeSettings {
                     ToolTypeSettings.DefinitionStages.WithPythonInterpreter<WithAttach<ParentT>> {
             }
 
+            /**
+             * The first stage of the Chainer settings definition.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface Blank<ParentT> extends WithPython<ParentT> {
             }
 
+            /**
+             * Specifies python script file path to execute the job.
+             * @param <ParentT> the stage of the parent definition to return to after attaching this definition
+             */
             interface WithPython<ParentT> {
                 WithAttachAndPythonInterpreter<ParentT> withPythonScriptFile(String pythonScriptFilePath);
             }
@@ -245,15 +305,36 @@ public interface ToolTypeSettings {
      * Common definition stages.
      */
     interface DefinitionStages {
+        /**
+         * Specifies number of processes parameter that is passed to MPI runtime.
+         * The default value for this property is equal to nodeCount property.
+         * @param <ReturnT> next definition stage type
+         */
         interface WithProcessCount<ReturnT> {
+            /**
+             * @param processCount Number of processes parameter that is passed to MPI runtime
+             * @return the next stage of the definition
+             */
             ReturnT withProcessCount(int processCount);
         }
 
+        /**
+         * Specifies command line arguments for the job.
+         * @param <ReturnT> next definition stage type
+         */
         interface WithCommandLineArgs<ReturnT> {
             ReturnT withCommandLineArgs(String commandLineArgs);
         }
 
+        /**
+         * Specifies the path to python interpreter.
+         * @param <ReturnT> next definition stage type
+         */
         interface WithPythonInterpreter<ReturnT> {
+            /**
+             * @param path the path to python interpreter
+             * @return the next stage of the definition
+             */
             ReturnT withPythonInterpreterPath(String path);
         }
     }

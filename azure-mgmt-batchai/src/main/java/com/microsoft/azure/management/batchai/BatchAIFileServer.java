fce718ff89b19b3535e0185a605d61b31b768110
lenala <lena.lakhno@gmail.com>
2018-01-10 15:46:27 -0800

    checkstyle

--- azure-mgmt-batchai/src/main/java/com/microsoft/azure/management/batchai/BatchAIFileServer.java
+++ azure-mgmt-batchai/src/main/java/com/microsoft/azure/management/batchai/BatchAIFileServer.java
@@ -12,10 +12,8 @@ import com.microsoft.azure.management.batchai.implementation.BatchAIManager;
 import com.microsoft.azure.management.batchai.implementation.FileServerInner;
 import com.microsoft.azure.management.resources.fluentcore.arm.models.GroupableResource;
 import com.microsoft.azure.management.resources.fluentcore.arm.models.Resource;
-import com.microsoft.azure.management.resources.fluentcore.model.Appliable;
 import com.microsoft.azure.management.resources.fluentcore.model.Creatable;
 import com.microsoft.azure.management.resources.fluentcore.model.Refreshable;
-import com.microsoft.azure.management.resources.fluentcore.model.Updatable;
 
 /**
  * Entry point for Batch AI file server management API in Azure.

e7577dbd120cfd6582e9202dc380f47c12facf4a
ro-joowan <jooro@microsoft.com>
2018-02-20 17:28:20 -0800

    [ADLA] Resolving checkstyle errors

--- azure-mgmt-datalake-analytics/src/main/java/com/microsoft/azure/management/datalake/analytics/models/SubResource.java
+++ azure-mgmt-datalake-analytics/src/main/java/com/microsoft/azure/management/datalake/analytics/models/SubResource.java
@@ -1,25 +1,59 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ *
+ */
+
 package com.microsoft.azure.management.datalake.analytics.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The resource model definition for a nested resource.
+ */
 public class SubResource {
+    /**
+     * The resource identifier.
+     */
     @JsonProperty(value = "id", access = JsonProperty.Access.WRITE_ONLY)
     private String id;
 
+    /**
+     * The resource name.
+     */
     @JsonProperty(value = "name", access = JsonProperty.Access.WRITE_ONLY)
     private String name;
 
+    /**
+     * The resource type.
+     */
     @JsonProperty(value = "type", access = JsonProperty.Access.WRITE_ONLY)
     private String type;
 
+    /**
+     * Get the id value.
+     *
+     * @return the id value
+     */
     public String id() {
         return this.id;
     }
 
+    /**
+     * Get the name value.
+     *
+     * @return the name value
+     */
     public String name() {
         return this.name;
     }
 
+    /**
+     * Get the type value.
+     *
+     * @return the type value
+     */
     public String type() {
         return this.type;
     }

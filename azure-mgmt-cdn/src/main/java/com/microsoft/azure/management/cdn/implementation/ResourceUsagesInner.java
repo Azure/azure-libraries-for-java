55ac3d08a4124b3e379b926a9806ab388101c0a5
Dan Schulte <daschult@microsoft.com>
2018-03-07 10:13:03 -0800

    Update generated code from fixed generator and fix fluent layer

--- azure-mgmt-cdn/src/main/java/com/microsoft/azure/management/cdn/implementation/ResourceUsagesInner.java
+++ azure-mgmt-cdn/src/main/java/com/microsoft/azure/management/cdn/implementation/ResourceUsagesInner.java
@@ -20,6 +20,7 @@ import com.microsoft.rest.ServiceResponse;
 import java.io.IOException;
 import java.util.List;
 import okhttp3.ResponseBody;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.Path;

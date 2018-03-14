b9c8a145bcbedd675d10086a7f35de7ae27f30a8
lenala <lena.lakhno@gmail.com>
2018-03-14 12:47:30 -0700

    Updating CI

--- ci/create_image.sh
+++ ci/create_image.sh
@@ -13,13 +13,13 @@ title 'Generating tasks list'
 java -jar ./ci/java-test-collector-1.0-SNAPSHOT.jar ./
 
 title 'Building docker image'
-image=azuresdk-a01-droid:java-$TRAVIS_BUILD_NUMBER
+image=azuresdk-java:java-$TRAVIS_BUILD_NUMBER
 docker build -t $image -f ci/Dockerfile .
 
 title 'Pushing docker image'
-image=azuresdk-a01-droid:java-$TRAVIS_BUILD_NUMBER
+image=azuresdk-java:java-$TRAVIS_BUILD_NUMBER
 docker login azureclidev.azurecr.io -u $AZURESDKDEV_ACR_SP_USERNAME -p $AZURESDK_ACR_SP_PASSWORD
-docker tag azuresdk-a01-droid:java-$TRAVIS_BUILD_NUMBER azureclidev.azurecr.io/azuresdk-a01-droid:java-$TRAVIS_BUILD_NUMBER
-docker tag azuresdk-a01-droid:java-$TRAVIS_BUILD_NUMBER azureclidev.azurecr.io/azuresdk-a01-droid:java-latest
-docker push azureclidev.azurecr.io/azuresdk-a01-droid:java-$TRAVIS_BUILD_NUMBER
-docker push azureclidev.azurecr.io/azuresdk-a01-droid:java-latest
+docker tag azuresdk-java:java-$TRAVIS_BUILD_NUMBER azureclidev.azurecr.io/azuresdk-java:java-$TRAVIS_BUILD_NUMBER
+docker tag azuresdk-java:java-$TRAVIS_BUILD_NUMBER azureclidev.azurecr.io/azuresdk-java:java-latest
+docker push azureclidev.azurecr.io/azuresdk-java:java-$TRAVIS_BUILD_NUMBER
+docker push azureclidev.azurecr.io/azuresdk-java:java-latest

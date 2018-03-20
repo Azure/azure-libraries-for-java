cc4719ae91a0397ec2dab32e173a8b8622f03678
Anu Thomas Chandy <anuamd@hotmail.com>
2018-03-20 10:15:36 -0700

    Support for setting  priority for vms in vmss

--- azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/implementation/VirtualMachineScaleSetImpl.java
+++ azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/implementation/VirtualMachineScaleSetImpl.java
@@ -26,6 +26,7 @@ import com.microsoft.azure.management.compute.StorageAccountTypes;
 import com.microsoft.azure.management.compute.UpgradeMode;
 import com.microsoft.azure.management.compute.UpgradePolicy;
 import com.microsoft.azure.management.compute.VirtualHardDisk;
+import com.microsoft.azure.management.compute.VirtualMachinePriorityTypes;
 import com.microsoft.azure.management.compute.VirtualMachineScaleSet;
 import com.microsoft.azure.management.compute.VirtualMachineScaleSetDataDisk;
 import com.microsoft.azure.management.compute.VirtualMachineScaleSetExtension;
@@ -400,6 +401,15 @@ public class VirtualMachineScaleSetImpl
     @Override
     public Map<String, VirtualMachineScaleSetExtension> extensions() {
         return Collections.unmodifiableMap(this.extensions);
+    }
+
+    @Override
+    public VirtualMachinePriorityTypes virtualMachinePriority() {
+        if (this.inner().virtualMachineProfile() != null) {
+            return this.inner().virtualMachineProfile().priority();
+        } else {
+            return null;
+        }
     }
 
     @Override
@@ -2151,6 +2161,12 @@ public class VirtualMachineScaleSetImpl
     @Override
     public VirtualMachineScaleSetImpl withoutBootDiagnostics() {
         this.bootDiagnosticsHandler.withoutBootDiagnostics();
+        return this;
+    }
+
+    @Override
+    public VirtualMachineScaleSetImpl withVirtualMachinePriority(VirtualMachinePriorityTypes priority) {
+        this.inner().virtualMachineProfile().withPriority(priority);
         return this;
     }
 

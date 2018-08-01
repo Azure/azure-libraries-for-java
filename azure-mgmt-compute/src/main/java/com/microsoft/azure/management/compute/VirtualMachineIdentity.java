5b69591ee79b394dc341a06538f360ad43d62350
Jianghao Lu <jianghaolu@users.noreply.github.com>
2018-07-31 18:01:47 -0700

    Use JsonInclude against Jackson's breaking change

--- azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/VirtualMachineIdentity.java
+++ azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/VirtualMachineIdentity.java
@@ -9,6 +9,9 @@
 package com.microsoft.azure.management.compute;
 
 import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
@@ -47,6 +50,7 @@ public class VirtualMachineIdentity {
      * '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/{identityName}'.
      */
     @JsonProperty(value = "userAssignedIdentities")
+    @JsonInclude(content = Include.ALWAYS)
     private Map<String, VirtualMachineIdentityUserAssignedIdentitiesValue> userAssignedIdentities;
 
     /**

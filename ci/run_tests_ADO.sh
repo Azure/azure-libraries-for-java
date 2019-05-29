1e05ae5d87348603f785bf2b70c3d5f2fa0c4bb6
Pareekshit Singh <pareekshit.singh@microsoft.com>
2019-05-29 12:26:21 -0700

    Add new script for runing tests in ADO and update the yml

---
+++ ci/run_tests_ADO.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+# ###########################################
+# Define colored output func
+function title {
+    LGREEN='\033[1;32m'
+    CLEAR='\033[0m'
+
+    echo -e ${LGREEN}$1${CLEAR}
+}
+
+#############################################
+#run jetty
+echo '==> Starting mvn jetty:run ===' ;
+mvn jetty:run &
+JETTY_PID=$!
+
+#############################################
+#wait for jetty to start
+echo '==> Waiting for jetty to start ===' ;
+sleep 10
+
+#############################################
+LOG_PARAMS='-Dorg.slf4j.simpleLogger.defaultLogLevel=error -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn --batch-mode' ;
+if [ ${TRAVIS_JDK_VERSION} = "oraclejdk8" ]; then
+    title 'Running checkstyle:check'
+    mvn checkstyle:check ;
+fi
+title 'Running mvn -pl !azure-samples package javadoc:aggregate -DskipTests=true $LOG_PARAMS'
+mvn -pl !azure-samples package javadoc:aggregate -DskipTests=true $LOG_PARAMS ;
+echo '==> Starting mvn test ===' ;
+mvn test -Dsurefire.rerunFailingTestsCount=3 $LOG_PARAMS -Dparallel=classes -DthreadCount=2 -DforkCount=1C ;
+
+#############################################
+#kill jetty
+echo '==> kill the jetty process ===' ;
+kill $JETTY_PID

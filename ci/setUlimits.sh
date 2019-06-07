c590ad88dddccfa0f0c12f44142f6d7875d9f4db
Pareekshit Singh <pareekshit.singh@microsoft.com>
2019-06-07 12:00:14 -0700

    modify ulimits script

--- ci/setUlimits.sh
+++ ci/setUlimits.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Current limits :"
 ulimit -n
 
 ulimit
 
-ulimit --help
-
+echo "Setting new limits"
 ulimit -S unlimited
 ulimit -H unlimited
 
+ulimit -n 65535
+
+echo "updated limits"
 ulimit
+
+ulimit -n

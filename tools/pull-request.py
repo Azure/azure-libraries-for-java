30b34a322a6737c3b79ba87056ef3387893a833f
ChenTanyi <chentanyi@gmail.com>
2019-11-11 10:26:56 +0800

    add: pull-request py

---
+++ tools/pull-request.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import os
+import sys
+import json
+import requests
+import urllib.parse
+
+def create_pull_request(url, title, head, auth = os.environ.get("GITHUB_TOKEN")):
+    url = urllib.parse.urljoin("https://api.github.com/repos/", url)
+    resp = requests.post(
+        url = url,
+        auth = (auth, ""),
+        json = {
+            "title": title,
+            "base": "master",
+            "head": head,
+        }
+    )
+    resp.raise_for_status()
+    print(json.dumps(resp.json(), indent = 2))
+
+def main(argv):
+    if len(argv) < 4:
+        print(f"Usage: {argv[0]} url title head [auth]")
+        print("        url: <owner>/<repo>/pulls[/<numbers]")
+        sys.exit(1)
+    create_pull_request(*argv[1:5])
+
+if __name__ == "__main__":
+    main(sys.argv)

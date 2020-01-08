63b1615f2b9762dcf76b6215b059cdeadb3ca809
Yaohai Zheng <yaozheng@microsoft.com>
2020-01-08 15:55:34 +0800

    Correct pom settings.

--- azure-mgmt-storage/src/test/java/com/azure/management/storage/StorageAccountOperationsTests.java
+++ azure-mgmt-storage/src/test/java/com/azure/management/storage/StorageAccountOperationsTests.java
@@ -14,7 +14,6 @@ import com.azure.management.resources.fluentcore.utils.Utils;
 import org.junit.Assert;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Map;

a3eb281127e76db1b105b3113e1706f249558e16
Yaohai Zheng <yaozheng@microsoft.com>
2020-01-23 21:53:27 +0800

    Add network V1 source code.

---
+++ azure-mgmt-network/src/main/java/com/microsoft/azure/management/network/ExpressRouteCrossConnection.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+package com.microsoft.azure.management.network;
+
+import com.microsoft.azure.management.apigeneration.Beta;
+import com.microsoft.azure.management.apigeneration.Fluent;
+import com.microsoft.azure.management.network.implementation.ExpressRouteCrossConnectionInner;
+import com.microsoft.azure.management.network.implementation.NetworkManager;
+import com.microsoft.azure.management.network.model.UpdatableWithTags;
+import com.microsoft.azure.management.resources.fluentcore.arm.models.GroupableResource;
+import com.microsoft.azure.management.resources.fluentcore.arm.models.Resource;
+import com.microsoft.azure.management.resources.fluentcore.model.Appliable;
+import com.microsoft.azure.management.resources.fluentcore.model.Refreshable;
+import com.microsoft.azure.management.resources.fluentcore.model.Updatable;
+
+import java.util.Map;
+
+/**
+ * Entry point for Express Route Cross Connection management API in Azure.
+ */
+@Fluent
+@Beta(Beta.SinceVersion.V1_11_0)
+public interface ExpressRouteCrossConnection extends
+        GroupableResource<NetworkManager, ExpressRouteCrossConnectionInner>,
+        Refreshable<ExpressRouteCrossConnection>,
+        Updatable<ExpressRouteCrossConnection.Update>,
+        UpdatableWithTags<ExpressRouteCrossConnection> {
+
+    /**
+     * @return entry point to manage express route peerings associated with express route circuit
+     */
+    ExpressRouteCrossConnectionPeerings peerings();
+    /**
+     * @return the name of the primary  port
+     */
+    String primaryAzurePort();
+
+    /**
+     * @return the name of the secondary  port
+     */
+    String secondaryAzurePort();
+
+    /**
+     * @return the identifier of the circuit traffic
+     */
+    Integer sTag();
+
+    /**
+     * @return the peering location of the ExpressRoute circuit
+     */
+    String peeringLocation();
+
+    /**
+     * @return the circuit bandwidth In Mbps
+     */
+    int bandwidthInMbps();
+
+    /**
+     * @return the ExpressRouteCircuit
+     */
+    ExpressRouteCircuitReference expressRouteCircuit();
+
+    /**
+     * @return the provisioning state of the circuit in the connectivity provider system
+     */
+    ServiceProviderProvisioningState serviceProviderProvisioningState();
+
+    /**
+     * @return additional read only notes set by the connectivity provider
+     */
+    String serviceProviderNotes();
+
+    /**
+     * @return the provisioning state of the express route cross connection resource
+     */
+    String provisioningState();
+
+    /**
+     * @return the peerings associated with this express route cross connection, indexed by name
+     */
+    Map<String, ExpressRouteCrossConnectionPeering> peeringsMap();
+
+    /**
+     * Grouping of express route cross connection update stages.
+     */
+    interface UpdateStages {
+        /**
+         * The stage of express route cross connection update allowing to specify service provider provisioning state.
+         */
+        interface WithServiceProviderProviosioningState {
+            Update withServiceProviderProvisioningState(ServiceProviderProvisioningState state);
+        }
+
+        /**
+         * The stage of express route cross connection update allowing to specify service provider notes.
+         */
+        interface WithServiceProviderNotes {
+            Update withServiceProviderNotes(String notes);
+        }
+    }
+
+    /**
+     * The template for a express route cross connection update operation, containing all the settings that
+     * can be modified.
+     */
+    interface Update extends
+            Appliable<ExpressRouteCrossConnection>,
+            Resource.UpdateWithTags<Update>,
+            UpdateStages.WithServiceProviderProviosioningState,
+            UpdateStages.WithServiceProviderNotes {
+    }
+}
---
+++ azure-mgmt-network/src/main/java/com/microsoft/azure/management/network/implementation/Ipv6PeeringConfigImpl.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+package com.microsoft.azure.management.network.implementation;
+
+import com.microsoft.azure.SubResource;
+import com.microsoft.azure.management.apigeneration.LangDefinition;
+import com.microsoft.azure.management.network.ExpressRouteCircuitPeeringConfig;
+import com.microsoft.azure.management.network.ExpressRouteCrossConnectionPeering;
+import com.microsoft.azure.management.network.Ipv6ExpressRouteCircuitPeeringConfig;
+import com.microsoft.azure.management.network.Ipv6PeeringConfig;
+import com.microsoft.azure.management.resources.fluentcore.model.implementation.IndexableWrapperImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  Implementation for Ipv6PeeringConfig.
+ */
+@LangDefinition
+class Ipv6PeeringConfigImpl
+        extends IndexableWrapperImpl<Ipv6ExpressRouteCircuitPeeringConfig>
+        implements
+        Ipv6PeeringConfig,
+        Ipv6PeeringConfig.Definition<ExpressRouteCrossConnectionPeering.DefinitionStages.WithCreate>,
+        Ipv6PeeringConfig.UpdateDefinition<ExpressRouteCrossConnectionPeering.Update>,
+        Ipv6PeeringConfig.Update {
+    private final ExpressRouteCrossConnectionPeeringImpl parent;
+    Ipv6PeeringConfigImpl(Ipv6ExpressRouteCircuitPeeringConfig innerObject, ExpressRouteCrossConnectionPeeringImpl parent) {
+        super(innerObject);
+        this.parent = parent;
+    }
+
+    @Override
+    public Ipv6PeeringConfigImpl withAdvertisedPublicPrefixes(List<String> publicPrefixes) {
+        ensureMicrosoftPeeringConfig().withAdvertisedPublicPrefixes(publicPrefixes);
+        return this;
+    }
+
+
+    @Override
+    public Ipv6PeeringConfigImpl withAdvertisedPublicPrefix(String publicPrefix) {
+        ExpressRouteCircuitPeeringConfig peeringConfig = ensureMicrosoftPeeringConfig();
+        if (peeringConfig.advertisedPublicPrefixes() == null) {
+            peeringConfig.withAdvertisedPublicPrefixes(new ArrayList<String>());
+        }
+        peeringConfig.advertisedPublicPrefixes().add(publicPrefix);
+        return this;
+    }
+
+    @Override
+    public Ipv6PeeringConfigImpl withPrimaryPeerAddressPrefix(String addressPrefix) {
+        inner().withPrimaryPeerAddressPrefix(addressPrefix);
+        return this;
+    }
+
+    @Override
+    public Ipv6PeeringConfigImpl withSecondaryPeerAddressPrefix(String addressPrefix) {
+        inner().withSecondaryPeerAddressPrefix(addressPrefix);
+        return this;
+    }
+
+    @Override
+    public Ipv6PeeringConfigImpl withCustomerASN(int customerASN) {
+        ensureMicrosoftPeeringConfig().withCustomerASN(customerASN);
+        return this;
+    }
+
+    @Override
+    public Ipv6PeeringConfigImpl withRouteFilter(String routeFilterId) {
+        inner().withRouteFilter(new SubResource().withId(routeFilterId));
+        return this;
+    }
+
+    @Override
+    public Ipv6PeeringConfigImpl withoutRouteFilter() {
+        inner().withRouteFilter(null);
+        return this;
+    }
+
+    @Override
+    public Ipv6PeeringConfigImpl withRoutingRegistryName(String routingRegistryName) {
+        ensureMicrosoftPeeringConfig().withRoutingRegistryName(routingRegistryName);
+        return this;
+    }
+
+    private ExpressRouteCircuitPeeringConfig ensureMicrosoftPeeringConfig() {
+        if (inner().microsoftPeeringConfig() == null) {
+            inner().withMicrosoftPeeringConfig(new ExpressRouteCircuitPeeringConfig());
+        }
+        return inner().microsoftPeeringConfig();
+    }
+
+    @Override
+    public ExpressRouteCrossConnectionPeeringImpl attach() {
+        return parent.attachIpv6Config(this);
+    }
+
+    @Override
+    public ExpressRouteCrossConnectionPeeringImpl parent() {
+        return parent;
+    }
+}

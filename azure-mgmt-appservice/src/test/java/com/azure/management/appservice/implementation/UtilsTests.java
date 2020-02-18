a1583b1547272ef9f122431e92a77b4ef92d7792
Weidong Xu <weidxu@microsoft.com>
2020-02-18 15:25:57 +0800

    update tests to junit 5

--- azure-mgmt-appservice/src/test/java/com/azure/management/appservice/implementation/UtilsTests.java
+++ azure-mgmt-appservice/src/test/java/com/azure/management/appservice/implementation/UtilsTests.java
@@ -14,30 +14,30 @@ public class UtilsTests {
     @Test
     public void testWebAppPrivateRegistryImage() throws Exception {
         // completion
-        Assert.assertEquals("weidxuregistry.azurecr.io/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("weidxuregistry.azurecr.io/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
+        Assertions.assertEquals("weidxuregistry.azurecr.io/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("weidxuregistry.azurecr.io/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
 
         // completion
-        Assert.assertEquals("weidxuregistry.azurecr.io/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("az-func-java:v1", "https://weidxuregistry.azurecr.io"));
+        Assertions.assertEquals("weidxuregistry.azurecr.io/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("az-func-java:v1", "https://weidxuregistry.azurecr.io"));
 
         // completion
-        Assert.assertEquals("weidxuregistry.azurecr.io/weidxu/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("weidxu/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
+        Assertions.assertEquals("weidxuregistry.azurecr.io/weidxu/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("weidxu/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
 
         // completion
-        Assert.assertEquals("weidxuregistry.azurecr.io:5000/weidxu/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("weidxu/az-func-java:v1", "https://weidxuregistry.azurecr.io:5000"));
+        Assertions.assertEquals("weidxuregistry.azurecr.io:5000/weidxu/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("weidxu/az-func-java:v1", "https://weidxuregistry.azurecr.io:5000"));
 
         // completion
-        Assert.assertEquals("weidxuregistry.azurecr.io/weidxu/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("az-func-java:v1", "https://weidxuregistry.azurecr.io/weidxu"));
+        Assertions.assertEquals("weidxuregistry.azurecr.io/weidxu/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("az-func-java:v1", "https://weidxuregistry.azurecr.io/weidxu"));
 
         // completion not happen due to possible host
-        Assert.assertEquals("host.name/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("host.name/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
+        Assertions.assertEquals("host.name/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("host.name/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
 
         // completion not happen due to possible port
-        Assert.assertEquals("host:port/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("host:port/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
+        Assertions.assertEquals("host:port/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("host:port/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
 
         // completion not happen due to no idea what it is
-        Assert.assertEquals("/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
+        Assertions.assertEquals("/az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("/az-func-java:v1", "https://weidxuregistry.azurecr.io"));
 
         // completion not happen due to incorrect serviceUrl
-        Assert.assertEquals("az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("az-func-java:v1", "weidxuregistry.azurecr.io"));
+        Assertions.assertEquals("az-func-java:v1", Utils.smartCompletionPrivateRegistryImage("az-func-java:v1", "weidxuregistry.azurecr.io"));
     }
 }

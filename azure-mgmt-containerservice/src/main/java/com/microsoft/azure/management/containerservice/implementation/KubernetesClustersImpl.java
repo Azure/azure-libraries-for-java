dea658743df837f3f6f92bd79f5619ca41a4af80
Tanyi Chen <tanchen@microsoft.com>
2020-03-26 14:57:43 +0800

    feat: deprecate getAccessProfile api (#1143)
    * feat: deprecate getAccessProfile api
    
    * fix: remove unused import
    
    * fix: change size == 0 -> empty
    
    * fix: aks test and update session record

--- azure-mgmt-containerservice/src/main/java/com/microsoft/azure/management/containerservice/implementation/KubernetesClustersImpl.java
+++ azure-mgmt-containerservice/src/main/java/com/microsoft/azure/management/containerservice/implementation/KubernetesClustersImpl.java
@@ -8,7 +8,6 @@ package com.microsoft.azure.management.containerservice.implementation;
 import com.microsoft.azure.PagedList;
 import com.microsoft.azure.management.apigeneration.LangDefinition;
 import com.microsoft.azure.management.containerservice.KubernetesCluster;
-import com.microsoft.azure.management.containerservice.KubernetesClusterAccessProfileRole;
 import com.microsoft.azure.management.containerservice.KubernetesClusters;
 import com.microsoft.azure.management.containerservice.OrchestratorVersionProfile;
 import com.microsoft.azure.management.resources.ResourceGroup;
@@ -157,25 +156,22 @@ public class KubernetesClustersImpl extends
 
     @Override
     public byte[] getAdminKubeConfigContent(String resourceGroupName, String kubernetesClusterName) {
-        ManagedClusterAccessProfileInner profileInner = this.manager().inner().managedClusters().getAccessProfile(resourceGroupName, kubernetesClusterName, KubernetesClusterAccessProfileRole.ADMIN.toString());
-        if (profileInner == null) {
-            return new byte[0];
-        } else {
-            return profileInner.kubeConfig();
-        }
+        return this.getAdminKubeConfigContentAsync(resourceGroupName, kubernetesClusterName).toBlocking().last();
     }
 
     @Override
     public Observable<byte[]> getAdminKubeConfigContentAsync(String resourceGroupName, String kubernetesClusterName) {
         return this.manager().inner().managedClusters()
-            .getAccessProfileAsync(resourceGroupName, kubernetesClusterName, KubernetesClusterAccessProfileRole.ADMIN.toString())
-            .map(new Func1<ManagedClusterAccessProfileInner, byte[]>() {
+            .listClusterAdminCredentialsAsync(resourceGroupName, kubernetesClusterName)
+            .map(new Func1<CredentialResultsInner, byte[]>() {
                 @Override
-                public byte[] call(ManagedClusterAccessProfileInner profileInner) {
-                    if (profileInner == null) {
+                public byte[] call(CredentialResultsInner credentialInner) {
+                    if (credentialInner == null
+                            || credentialInner.kubeconfigs() == null
+                            || credentialInner.kubeconfigs().isEmpty()) {
                         return new byte[0];
                     } else {
-                        return profileInner.kubeConfig();
+                        return credentialInner.kubeconfigs().get(0).value();
                     }
                 }
             });
@@ -183,25 +179,22 @@ public class KubernetesClustersImpl extends
 
     @Override
     public byte[] getUserKubeConfigContent(String resourceGroupName, String kubernetesClusterName) {
-        ManagedClusterAccessProfileInner profileInner = this.manager().inner().managedClusters().getAccessProfile(resourceGroupName, kubernetesClusterName, KubernetesClusterAccessProfileRole.USER.toString());
-        if (profileInner == null) {
-            return new byte[0];
-        } else {
-            return profileInner.kubeConfig();
-        }
+        return this.getUserKubeConfigContentAsync(resourceGroupName, kubernetesClusterName).toBlocking().last();
     }
 
     @Override
     public Observable<byte[]> getUserKubeConfigContentAsync(String resourceGroupName, String kubernetesClusterName) {
         return this.manager().inner().managedClusters()
-            .getAccessProfileAsync(resourceGroupName, kubernetesClusterName, KubernetesClusterAccessProfileRole.USER.toString())
-            .map(new Func1<ManagedClusterAccessProfileInner, byte[]>() {
+            .listClusterUserCredentialsAsync(resourceGroupName, kubernetesClusterName)
+            .map(new Func1<CredentialResultsInner, byte[]>() {
                 @Override
-                public byte[] call(ManagedClusterAccessProfileInner profileInner) {
-                    if (profileInner == null) {
+                public byte[] call(CredentialResultsInner credentialInner) {
+                    if (credentialInner == null
+                        || credentialInner.kubeconfigs() == null
+                        || credentialInner.kubeconfigs().isEmpty()) {
                         return new byte[0];
                     } else {
-                        return profileInner.kubeConfig();
+                        return credentialInner.kubeconfigs().get(0).value();
                     }
                 }
             });

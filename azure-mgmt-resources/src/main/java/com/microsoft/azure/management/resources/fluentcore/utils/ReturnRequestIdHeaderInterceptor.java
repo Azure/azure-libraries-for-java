4b16e058535289435ab28e5cc20a4bfce14a2f5d
Weidong Xu <weidxu@microsoft.com>
2020-08-12 12:29:38 +0800

    improve doc, use enum for readability

--- azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/utils/ReturnRequestIdHeaderInterceptor.java
+++ azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/utils/ReturnRequestIdHeaderInterceptor.java
@@ -6,6 +6,8 @@
 
 package com.microsoft.azure.management.resources.fluentcore.utils;
 
+import com.microsoft.rest.RestClient;
+import com.microsoft.rest.interceptors.RequestIdHeaderInterceptor;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -15,37 +17,58 @@ import java.io.IOException;
 /**
  * An interceptor for requesting server return client-request-id in response headers.
  * Optionally, fill-in the client-request-id, if server does not return it in response headers.
+ * <p>
+ * ReturnRequestIdHeaderInterceptor should be added after {@link RequestIdHeaderInterceptor}.
+ * By default, {@link RequestIdHeaderInterceptor} is added as first interceptor by {@link RestClient}.
+ *
+ * @see RequestIdHeaderInterceptor
  */
 public class ReturnRequestIdHeaderInterceptor implements Interceptor {
 
     private static final String NAME_RETURN_CLIENT_REQUEST_ID = "x-ms-return-client-request-id";
     private static final String NAME_CLIENT_REQUEST_ID = "x-ms-client-request-id";
 
-    private final boolean copyClientRequestIdInResponse;
+    private final Option option;
+
+    /**
+     * Additional client handling, if server does not return client-request-id in response headers.
+     */
+    public enum Option {
+        /**
+         * Default.
+         */
+        NONE,
+
+        /**
+         * Fill-in the client-request-id from request headers.
+         */
+        COPY_CLIENT_REQUEST_ID
+    }
 
     /**
      * Creates a new instance of ReturnRequestIdHeaderInterceptor.
      * Sets "x-ms-return-client-request-id: true" in requests headers.
      */
     public ReturnRequestIdHeaderInterceptor() {
-        this(false);
+        this(Option.NONE);
     }
 
     /**
      * Creates a new instance of ReturnRequestIdHeaderInterceptor.
      * Sets "x-ms-return-client-request-id: true" in requests headers.
+     * <p>
      * Optionally fill-in the client-request-id if server does not return it in response headers.
      *
-     * @param copyClientRequestIdInResponse whether to copy client-request-id in request headers to response headers, if server does not return client-request-id.
+     * @param option the option of additional client handling, if server does not return client-request-id in response headers.
      */
-    public ReturnRequestIdHeaderInterceptor(boolean copyClientRequestIdInResponse) {
-        this.copyClientRequestIdInResponse = copyClientRequestIdInResponse;
+    public ReturnRequestIdHeaderInterceptor(Option option) {
+        this.option = option;
     }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
-        String clientRequestId = request.header(NAME_CLIENT_REQUEST_ID);
+        final String clientRequestId = request.header(NAME_CLIENT_REQUEST_ID);
 
         // add header if absent
         if (request.header(NAME_RETURN_CLIENT_REQUEST_ID) == null) {
@@ -57,8 +80,9 @@ public class ReturnRequestIdHeaderInterceptor implements Interceptor {
         // send request
         Response response = chain.proceed(request);
 
-        // optionally, add header if absent
-        if (copyClientRequestIdInResponse && clientRequestId != null && response.header(NAME_CLIENT_REQUEST_ID) == null) {
+        // optionally, copy header from request, if absent in response
+        if (option == Option.COPY_CLIENT_REQUEST_ID
+                && clientRequestId != null && response.header(NAME_CLIENT_REQUEST_ID) == null) {
             response = response.newBuilder()
                     .addHeader(NAME_CLIENT_REQUEST_ID, clientRequestId)
                     .build();

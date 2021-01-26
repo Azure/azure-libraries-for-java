a80fcc8dfbe86d74fd083f88c185c8b12766c189
xccc-msft <chuxi@microsoft.com>
2021-01-26 17:03:04 +0800

    generate subscriptions

--- azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/implementation/SubscriptionClientImpl.java
+++ azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/implementation/SubscriptionClientImpl.java
@@ -129,6 +129,19 @@ public class SubscriptionClientImpl extends AzureServiceClient {
     }
 
     /**
+     * The OperationsInner object to access its operations.
+     */
+    private OperationsInner operations;
+
+    /**
+     * Gets the OperationsInner object to access its operations.
+     * @return the OperationsInner object.
+     */
+    public OperationsInner operations() {
+        return this.operations;
+    }
+
+    /**
      * The SubscriptionsInner object to access its operations.
      */
     private SubscriptionsInner subscriptions;
@@ -189,6 +202,7 @@ public class SubscriptionClientImpl extends AzureServiceClient {
         this.acceptLanguage = "en-US";
         this.longRunningOperationRetryTimeout = 30;
         this.generateClientRequestId = true;
+        this.operations = new OperationsInner(restClient().retrofit(), this);
         this.subscriptions = new SubscriptionsInner(restClient().retrofit(), this);
         this.tenants = new TenantsInner(restClient().retrofit(), this);
         this.azureClient = new AzureClient(this);

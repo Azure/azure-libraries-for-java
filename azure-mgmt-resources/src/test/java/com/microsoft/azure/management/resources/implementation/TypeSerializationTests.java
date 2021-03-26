f888f72482f590d78078c6fb1be003d55de323c1
Weidong Xu <weidxu@microsoft.com>
2021-03-26 12:11:47 +0800

    test case for TagsPatchResource serialization

--- azure-mgmt-resources/src/test/java/com/microsoft/azure/management/resources/implementation/TypeSerializationTests.java
+++ azure-mgmt-resources/src/test/java/com/microsoft/azure/management/resources/implementation/TypeSerializationTests.java
@@ -7,14 +7,83 @@
 package com.microsoft.azure.management.resources.implementation;
 
 import com.microsoft.azure.management.resources.DeploymentProperties;
+import com.microsoft.azure.management.resources.Tags;
+import com.microsoft.azure.management.resources.TagsPatchOperation;
+import com.microsoft.azure.management.resources.TagsPatchResource;
 import com.microsoft.azure.serializer.AzureJacksonAdapter;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class TypeSerializationTests {
+
+    private static final class Map1<K,V> extends AbstractMap<K,V> {
+        private final K k0;
+        private final V v0;
+
+        Map1(K k0, V v0) {
+            this.k0 = Objects.requireNonNull(k0);
+            this.v0 = Objects.requireNonNull(v0);
+        }
+
+        @Override
+        public Set<Entry<K,V>> entrySet() {
+            Entry<K,V> entry = new AbstractMap.SimpleEntry<>(k0, v0);
+            return new HashSet<>(Collections.singletonList(entry));
+        }
+
+        @Override
+        public V get(Object o) {
+            return o.equals(k0) ? v0 : null;
+        }
+
+        @Override
+        public boolean containsKey(Object o) {
+            return o.equals(k0);
+        }
+
+        @Override
+        public boolean containsValue(Object o) {
+            return o.equals(v0); // implicit nullcheck of o
+        }
+
+        @Override
+        public int size() {
+            return 1;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return k0.hashCode() ^ v0.hashCode();
+        }
+    }
+
+    @Test
+    @Ignore("fails if Map is final")
+    public void testTagsPatchResourceSerialization() throws Exception {
+        Map<String, String> tags = new Map1<>("tag.1", "value.1");
+
+        TagsPatchResource tagsPatchResource = new TagsPatchResource()
+                .withOperation(TagsPatchOperation.REPLACE)
+                .withProperties(new Tags().withTags(tags));
+
+        AzureJacksonAdapter serializerAdapter = new AzureJacksonAdapter();
+        String tagsPatchResourceJson = serializerAdapter.serialize(tagsPatchResource);
+        Assert.assertTrue(tagsPatchResourceJson.contains("tag.1"));
+    }
 
     @Test
     public void testDeploymentSerialization() throws Exception {
